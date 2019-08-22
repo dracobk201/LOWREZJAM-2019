ab75aac6124058149224e58d650cf09a2ecdb17a
Erick <dracobk201@gmail.com>
2019-08-22 16:19:37 -0400

    - main menu's structure.

---
+++ LOWREZJAM 2019/Assets/Scripts/HolderBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolderBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private FloatReference TimeForWave;
+    [SerializeField]
+    private FloatReference RemainingTime;
+    [SerializeField]
+    private FloatReference TargetPercentage;
+    private float lastPercentage;
+
+    private void Start()
+    {
+        lastPercentage = 100;
+    }
+
+    private void TimeChecker()
+    {
+        float actualPercentage = (RemainingTime.Value * 100f) / TimeForWave.Value;
+        //Debug.Log(actualPercentage + " - "+ targetPercentage);
+        if (actualPercentage <= TargetPercentage.Value)
+        {
+            //GetComponent<SpriteRenderer>().color = 
+            //Mathf.
+        }
+    }
+}
